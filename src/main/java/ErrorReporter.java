f5ed3f39b92f361463f74203090b350406d5036a
David <davidcha@email.unc.edu>
2024-01-26 20:56:50 -0500

    init required classes

---
+++ src/main/java/ErrorReporter.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+public class ErrorReporter {
+    private List<String> _errorQueue;
+
+    public ErrorReporter() {
+        this._errorQueue = new ArrayList<>();
+    }
+
+    public boolean hasErrors() {
+        return !this._errorQueue.isEmpty();
+    }
+
+    public void reportError(String ...error) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : error) {
+            sb.append(s);
+        }
+
+        _errorQueue.add(sb.toString());
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (this.hasErrors()) {
+            for (String s : _errorQueue) {
+                sb.append(s);
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+}
