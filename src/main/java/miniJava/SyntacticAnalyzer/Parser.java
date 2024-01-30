2a3709ff72268c42f9004c1aea7f62b4d86dfaa3
David <davidcha@email.unc.edu>
2024-01-30 03:05:43 -0500

    I think I finished the parser recursive descent method definitions

--- src/main/java/miniJava/SyntacticAnalyzer/Parser.java
+++ src/main/java/miniJava/SyntacticAnalyzer/Parser.java
@@ -3,34 +3,106 @@ package miniJava.SyntacticAnalyzer;
 import miniJava.ErrorReporter;
 
 public class Parser implements ParserInterface {
-    private Lexer _lexer;
-    private Token _currToken;
-    private ErrorReporter _reporter;
+    private Lexer lexer;
+    private Token currToken;
+    private ErrorReporter reporter;
     private boolean eot = false;
 
     public Parser(Lexer lexer, ErrorReporter reporter) {
-        this._lexer = lexer;
-        this._reporter = reporter;
+        this.lexer = lexer;
+        this.reporter = reporter;
     }
     @Override
     public void parse() {
-        _currToken = _lexer.scan();
+        currToken = lexer.scan();
         parseClassDecl();
-        while (this._currToken.getType() == TokenType.CLASS) {
+        while (currToken.getType() == TokenType.CLASS) {
+            accept(TokenType.CLASS);
             parseClassDecl();
         }
     }
 
     private void parseClassDecl() {
-        accept(TokenType.CLASS);
+        if (currToken.getType() != TokenType.CLASS) {
+            rejectIt();
+            return;
+        }
+        parseIdentifier();
+        accept(TokenType.LCURLY);
+        parseFieldDecl();
+        accept(TokenType.RCURLY);
+    }
+
+    private void methodDecl() {
+        parseVis();
+        parseAccess();
+        if (currToken.getType() == TokenType.VOID) {
+
+        } else parseType();
+        parseIdentifier();
+        accept(TokenType.LPAREN);
+        // parameterList?
+        parseParamList();
+    }
+
+    private void parseParamList() {
+        parseType();
+        parseIdentifier();
+        // (, Expression)*
+    }
+
+    private void parseArgList() {
+        parseExpr();
+    }
+
+    private void parseFieldDecl() {
+        parseVis();
+        parseAccess();
+        parseType();
+        parseIdentifier();
+    }
+
+    private void parseType() {
+        // int | boolean | (int | id)[]
+    }
+
+    private void parseAccess() {
+        // static ?
+    }
+
+    private void parseVis() {
+        // public | private ?
+    }
+
+    private void parseIdentifier() {
+        if (currToken.getType() != TokenType.IDENTIFIER) {
+            rejectIt();
+            return;
+        }
+    }
+
+    private void parseRef() {
+        // id | this | Reference.id
+    }
+
+    private void parseStatement() {
+
+    }
+    private void parseExpr() {
+
     }
 
     private void accept(TokenType expect) {
-        if (_currToken.getType() == expect) {
-           _currToken= _lexer.scan();
+        if (currToken.getType() == expect) {
+           currToken= lexer.scan();
         }
     }
 
     private void rejectIt() {
+        parseError();
+    }
+
+    private void parseError() {
+        reporter.reportError("Parse error: ");
     }
 }
