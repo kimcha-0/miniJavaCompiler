3adec1eafede6ff4cf3ca26e7771e3e36f574b0b
David <davidcha@email.unc.edu>
2024-02-04 22:24:53 -0500

    ignoring test files for git

--- src/main/java/miniJava/SyntacticAnalyzer/Lexer.java
+++ src/main/java/miniJava/SyntacticAnalyzer/Lexer.java
@@ -111,8 +111,7 @@ public class Lexer implements LexerInterface {
                 return TokenType.OPERATOR;
                 // indefinite lexemes
             case '/':
-                TokenType slashResult = handleSlash();
-                return slashResult;
+                return handleSlash();
             case '0': case '1': case '2': case '3': case '4':
             case '5': case '6': case '7': case '8': case '9': 
                 while (isDigit(charBuf)) {
