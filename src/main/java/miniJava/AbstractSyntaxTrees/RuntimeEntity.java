00b17635f014289fe369a6443ca518aa828eb7a2
David <davidcha@email.unc.edu>
2024-04-25 01:46:54 -0400

    a.out and RuntimeEntity only stores offset.

---
+++ src/main/java/miniJava/AbstractSyntaxTrees/RuntimeEntity.java
@@ -0,0 +1,15 @@
+package miniJava.AbstractSyntaxTrees;
+
+public class RuntimeEntity {
+    public int offset;
+    public int size;
+
+    public RuntimeEntity() {
+    }
+
+
+    public int getSize() { return this.size; }
+    public int getOffset() {
+        return this.offset;
+    }
+}
